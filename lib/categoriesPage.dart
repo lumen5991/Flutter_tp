ccc44f587ad05d69089ffb5e4e7152d429e4dc3a
lumen5991 <lumenahle@gmail.com>
2024-02-11 02:37:21 +0100

    0237

--- lib/categoriesPage.dart
+++ lib/categoriesPage.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:new_project/headerWidget.dart';
+import 'package:flutter_appli_one/headerWidget.dart';
 
 class CategoriesPage extends StatefulWidget {
   const CategoriesPage({Key? key});
